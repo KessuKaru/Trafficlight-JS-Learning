5c7894537c21c97bca8cf2d5c50dbf20b5d364ba
Kessu Karu <kessulorien@gmail.com>
2021-10-07 22:44:10 +0200

    Added eslint

--- src/script.js
+++ src/script.js
@@ -1,22 +1,22 @@
 class Light {
   constructor(colour, parent) {
-    this.offColour = "gray";
+    this.offColour = 'gray';
     this.onColour = colour;
     this.currentColour = this.offColour;
-    this.html = document.createElement("span");
+    this.html = document.createElement('span');
 
-    this.html.className = "light";
+    this.html.className = 'light';
     parent.appendChild(this.html);
   }
 
   on() {
     this.currentColour = this.onColour;
-    this.renderToDOM()
+    this.renderToDOM();
   }
 
   off() {
     this.currentColour = this.offColour;
-    this.renderToDOM()
+    this.renderToDOM();
   }
 
   renderToDOM() {
@@ -26,19 +26,18 @@ class Light {
 }
 
 
-
 class TrafficLight extends HTMLElement {
   constructor() {
     super();
 
     const shadow = this.attachShadow({mode: 'open'});
-    
-    const html = document.createElement("div");
-    html.className = "ampel";
 
-    this.redlight = new Light("red", html);
-    this.yellowlight = new Light("yellow", html);
-    this.greenlight = new Light("green", html);
+    const html = document.createElement('div');
+    html.className = 'ampel';
+
+    this.redlight = new Light('red', html);
+    this.yellowlight = new Light('yellow', html);
+    this.greenlight = new Light('green', html);
 
     const style = document.createElement('style');
 
@@ -78,29 +77,29 @@ class TrafficLight extends HTMLElement {
   // If in RED state, switches to redYellow state
   // and then after timeYellow to green state.
   switchToGreen() {
- 
+
   }
 
   // If in GREEN state, switches to Yellow state
   // and then after timeYellow to green state.
   switchToRed() {
-    
+
   }
 
   // This state machine controls the trafic light colour changes
   advanceStatemachine(event) {
+
+    // Handle the outputs (lights on and off) based on the state after
+    // the transition
     
-    // Handle the outputs (lights on and off) based on the state after the transition
     // Outputs: redlight on/off, yellowlight on/off, greenlight on/off, timerTrigger, busy, enable
-    
-    // States: OFF, RED, REDYELLOW, GREEN, YELLOW, 
-    
+
+    // States: OFF, RED, REDYELLOW, GREEN, YELLOW,
+
     // Handle the state transitions based on the input event and the current state
     // Inputs: switchToGreen, switchToRed, timerDone, enable, disable
 
   }
-
-
 
 
   enterRedState() {
@@ -134,7 +133,7 @@ class TrafficLight extends HTMLElement {
   }
 }
 
-customElements.define("traffic-light", TrafficLight);
+customElements.define('traffic-light', TrafficLight);
 
 // let trafficLight1 = new TrafficLight("")
 // trafficLight1.run()
